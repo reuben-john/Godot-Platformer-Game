e06cbc9e3b9d274755629c89c7eaf27a229ffc61
Reuben <rjkoppy@gmail.com>
2022-10-07 11:39:52 -0400

    Change to enum for state and expand level

--- Steve.gd
+++ Steve.gd
@@ -1,35 +1,57 @@
 extends KinematicBody2D
 
+enum States {AIR, FLOOR, LADDER, WALL}
+var state = States.AIR
+
 var velocity = Vector2(0, 0)
 const SPEED = 380
 const GRAVITY = 35
-const JUMPFORCE = -1200
+const JUMPFORCE = -900
 
 func _physics_process(delta):
-	if Input.is_action_pressed("right"):
-		velocity.x = SPEED
-		$Sprite.play("walk")
-		$Sprite.flip_h = false;
-	elif Input.is_action_pressed("left"):
-		velocity.x = -SPEED
-		$Sprite.play("walk")
-		$Sprite.flip_h = true;
-	else:
-		$Sprite.play("idle")
-		
-	if not is_on_floor():
-		$Sprite.play("air")
-		
+	
+	match state:
+		States.AIR:
+			if is_on_floor():
+				state = States.FLOOR
+				continue
+			$Sprite.play("air")
+			if Input.is_action_pressed("right"):
+				velocity.x = SPEED
+				$Sprite.flip_h = false;
+			elif Input.is_action_pressed("left"):
+				velocity.x = -SPEED
+				$Sprite.flip_h = true;
+			else:
+				velocity.x = lerp(velocity.x, 0, 0.2)
+			move_and_fall()
+		States.FLOOR:
+			if not is_on_floor():
+				state = States.AIR
+				continue
+			if Input.is_action_pressed("right"):
+				velocity.x = SPEED
+				$Sprite.play("walk")
+				$Sprite.flip_h = false;
+			elif Input.is_action_pressed("left"):
+				velocity.x = -SPEED
+				$Sprite.play("walk")
+				$Sprite.flip_h = true;
+			else:
+				$Sprite.play("idle")
+				velocity.x = lerp(velocity.x, 0, 0.2)
+				
+			if Input.is_action_just_pressed("jump"):
+				velocity.y = JUMPFORCE
+				$SoundJump.play()
+				state = States.AIR
+			move_and_fall()
+	
+
+
+func move_and_fall():
 	velocity.y = velocity.y + GRAVITY
-	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMPFORCE
-		$SoundJump.play()
-		
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
-	velocity.x = lerp(velocity.x, 0, 0.2)
-#	velocity.y = lerp(velocity.y, 0, 0.2)
 
 
 func _on_Fallzone_body_entered(body):
